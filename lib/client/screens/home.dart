aaf9320401237687dfb1f2e5e559df4e9fd43265
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2023-01-25 18:19:16 +0000

    Prevent quiting app by back button android

--- lib/client/screens/home.dart
+++ lib/client/screens/home.dart
@@ -28,110 +28,113 @@ class Home extends StatelessWidget {
           value: BlocProvider.of<ElectionJustEndedBloc>(context)..add(LoadElectionJustEnded(elections: Election.elections)),
         ),
       ],
-      child: Scaffold(
-          backgroundColor: const Color.fromARGB(255, 246, 248, 250),
-          appBar: const CustomAppBar(back: false, disableBackGuard: false, disableMenu: false),
-          endDrawer: const CustomDrawer(),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Container(
-              //   margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-              //   child: Container(
-              //     height: 60,
-              //     padding: const EdgeInsets.all(8.0),
-              //     child: const TextField(
-              //         decoration: InputDecoration(
-              //       labelText: 'Type here to search',
-              //       suffixIcon: Icon(Icons.search),
-              //       enabledBorder: OutlineInputBorder(
-              //         borderRadius: BorderRadius.all(Radius.circular(6)),
-              //         borderSide: BorderSide(width: 0, color: Colors.white),
-              //       ),
-              //       fillColor: Color.fromARGB(255, 217, 217, 217),
-              //       filled: true,
-              //       suffixIconColor: Color.fromARGB(255, 255, 255, 255),
-              //       labelStyle: TextStyle(color: Colors.black),
-              //     )),
-              //   ),
-              // ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Column(children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5.0),
-                    child: Text(
-                      'Just ended:',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+      child: WillPopScope(
+        onWillPop: null,
+        child: Scaffold(
+            backgroundColor: const Color.fromARGB(255, 246, 248, 250),
+            appBar: const CustomAppBar(back: false, disableBackGuard: false, disableMenu: false),
+            endDrawer: const CustomDrawer(),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Container(
+                //   margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                //   child: Container(
+                //     height: 60,
+                //     padding: const EdgeInsets.all(8.0),
+                //     child: const TextField(
+                //         decoration: InputDecoration(
+                //       labelText: 'Type here to search',
+                //       suffixIcon: Icon(Icons.search),
+                //       enabledBorder: OutlineInputBorder(
+                //         borderRadius: BorderRadius.all(Radius.circular(6)),
+                //         borderSide: BorderSide(width: 0, color: Colors.white),
+                //       ),
+                //       fillColor: Color.fromARGB(255, 217, 217, 217),
+                //       filled: true,
+                //       suffixIconColor: Color.fromARGB(255, 255, 255, 255),
+                //       labelStyle: TextStyle(color: Colors.black),
+                //     )),
+                //   ),
+                // ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Column(children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5.0),
+                      child: Text(
+                        'Just ended:',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                      ),
                     ),
-                  ),
-                  BlocBuilder<ElectionJustEndedBloc, ElectionJustEndedState>(
-                    builder: (context, state) {
-                      if (state is ElectionJustEndedInitial) {
-                        return const Center(
-                          child: CircularProgressIndicator(
+                    BlocBuilder<ElectionJustEndedBloc, ElectionJustEndedState>(
+                      builder: (context, state) {
+                        if (state is ElectionJustEndedInitial) {
+                          return const Center(
+                            child: CircularProgressIndicator(
+                              color: Colors.blue,
+                            ),
+                          );
+                        } else if (state is ElectionJustEndedLoaded) {
+                          return ElectionCard(id: state.election.id, electionTitle: state.election.title, electionDescription: state.election.description, electionOrganization: state.election.organization, status: state.status, userId: user.id);
+                        } else {
+                          return const Text('Something is wrong');
+                        }
+                      },
+                    ),
+                  ]),
+                ),
+                Expanded(
+                  // margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: Column(children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5.0),
+                      child: Text(
+                        'Active joined election(s):',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                    BlocBuilder<JoinedElectionsBloc, JoinedElectionsState>(
+                      builder: (context, state) {
+                        if (state is JoinedElectionsInitial) {
+                          return const CircularProgressIndicator(
                             color: Colors.blue,
-                          ),
-                        );
-                      } else if (state is ElectionJustEndedLoaded) {
-                        return ElectionCard(id: state.election.id, electionTitle: state.election.title, electionDescription: state.election.description, electionOrganization: state.election.organization, status: state.status, userId: user.id);
-                      } else {
-                        return const Text('Something is wrong');
-                      }
-                    },
-                  ),
-                ]),
-              ),
-              Expanded(
-                // margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                child: Column(children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5.0),
-                    child: Text(
-                      'Active joined election(s):',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                          );
+                        } else if (state is JoinedElectionsLoaded) {
+                          // debugPrint('//${state.elections.length}');
+                          return Expanded(
+                            child: ListView.builder(
+                              padding: const EdgeInsets.only(bottom: 100),
+                              itemCount: state.elections.length,
+                              itemBuilder: (_, index) {
+                                Election key = state.elections.keys.elementAt(index);
+
+                                if (state.elections[key] == ElectionStatusEnum.voteClosed) {
+                                  return Container();
+                                }
+
+                                return ElectionCard(
+                                  id: key.id,
+                                  electionTitle: key.title,
+                                  electionDescription: key.description,
+                                  electionOrganization: key.organization,
+                                  status: state.elections[key]!,
+                                  userId: user.id,
+                                );
+                              },
+                            ),
+                          );
+                        } else {
+                          return const Text('Something is wrong');
+                        }
+                      },
                     ),
-                  ),
-                  BlocBuilder<JoinedElectionsBloc, JoinedElectionsState>(
-                    builder: (context, state) {
-                      if (state is JoinedElectionsInitial) {
-                        return const CircularProgressIndicator(
-                          color: Colors.blue,
-                        );
-                      } else if (state is JoinedElectionsLoaded) {
-                        // debugPrint('//${state.elections.length}');
-                        return Expanded(
-                          child: ListView.builder(
-                            padding: const EdgeInsets.only(bottom: 100),
-                            itemCount: state.elections.length,
-                            itemBuilder: (_, index) {
-                              Election key = state.elections.keys.elementAt(index);
-
-                              if (state.elections[key] == ElectionStatusEnum.voteClosed) {
-                                return Container();
-                              }
-
-                              return ElectionCard(
-                                id: key.id,
-                                electionTitle: key.title,
-                                electionDescription: key.description,
-                                electionOrganization: key.organization,
-                                status: state.elections[key]!,
-                                userId: user.id,
-                              );
-                            },
-                          ),
-                        );
-                      } else {
-                        return const Text('Something is wrong');
-                      }
-                    },
-                  ),
-                ]),
-              ),
-            ],
-          )),
+                  ]),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
