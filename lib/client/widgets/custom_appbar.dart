c2f42a72c6c929b750e13e42a8061143406225fb
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2023-04-10 21:22:28 +0100

    Refactor code

--- lib/client/widgets/custom_appbar.dart
+++ lib/client/widgets/custom_appbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+// Appbar of the app
 class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   final Size preferredSize;
@@ -28,41 +29,11 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
       shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))),
       toolbarHeight: 100,
       leadingWidth: 100,
-      shadowColor: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.3), //color of shadow
-      // spreadRadius: 0, //spread radius
-      // blurRadius: 50, // blur radius
-      // offset: const Offset(0, 4)),,
+      shadowColor: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.3),
       automaticallyImplyLeading: false,
       leading: back
           ? InkWell(
-              onTap: () {
-                if (disableBackGuard == false) {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Are you sure?'),
-                        content: const Text('Changes will not be saved.'),
-                        actions: [
-                          TextButton(
-                            onPressed: () => context.pop(),
-                            child: const Text('Cancel'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              context.pop();
-                              context.pop();
-                            },
-                            child: const Text('OK'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                } else {
-                  context.pop();
-                }
-              },
+              onTap: () => backButtonPressed(disableBackGuard, context),
               child: Container(
                 margin: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
                 padding: const EdgeInsets.symmetric(horizontal: 0.0),
@@ -101,5 +72,34 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
               ),
       ],
     );
+  }
+
+  void backButtonPressed(bool disableBackGuard, BuildContext context) {
+    if (disableBackGuard == false) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Are you sure?'),
+            content: const Text('Changes will not be saved.'),
+            actions: [
+              TextButton(
+                onPressed: () => context.pop(),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  context.pop();
+                  context.pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      context.pop();
+    }
   }
 }
